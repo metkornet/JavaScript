ab10a69d0acf4da31edec07bba52fb8e15335866
metkornet <60342504+metkornet@users.noreply.github.com>
2020-07-09 02:40:58 +0300

    Add class for menu card

--- project2/js/main.js
+++ project2/js/main.js
@@ -130,7 +130,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
    //создание появления модального окна через определенное количество времени (15 секунд)
 
-   const modalTimerId = setTimeout(openModel, 5000);
+//    const modalTimerId = setTimeout(openModel, 5000);
 
    // создание появления модального окна, когда пользователь долистал страницу до конца
     function showModalByScroll(){
@@ -141,4 +141,67 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
    window.addEventListener('scroll', showModalByScroll);
+
+   // создание классов для меню (карточки)
+   
+   class MenuCard{
+       constructor(src, alt, title, descrip, price, parentSelector){
+         this.src = src;
+         this.alt = alt;
+         this.title = title;
+         this.descrip = descrip;
+         this.price = price;
+         this.parent = document.querySelector(parentSelector);
+         this.transfer = 27;
+         this.changeToUAH();
+        }
+
+        changeToUAH(){
+            this.price = this.price*this.transfer;
+        }
+
+        render(){
+            const element = document.createElement('div');
+            element.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descrip}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>`;
+            this.parent.append(element);
+        }
+   }
+
+   new MenuCard(
+       "img/tabs/elite.jpg",
+        "elite",
+        'Меню "Постное”',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        9,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+         "vegy",
+         'Меню "Фитнес”',
+         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+         18,
+         ".menu .container"
+     ).render();
+
+     new MenuCard(
+        "img/tabs/elite.jpg",
+         "elite",
+         'Меню “Премиум”',
+         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+         21,
+         ".menu .container"
+     ).render();
+
 });
