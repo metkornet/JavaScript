3e95c7a53cabecc90737121327d720d2ff38d95e
metkornet <60342504+metkornet@users.noreply.github.com>
2020-07-10 12:29:10 +0300

    Converter currency

---
+++ calc/js/script.js
@@ -0,0 +1,18 @@
+const inputUSD = document.querySelector('#usd'),
+      inputRUB = document.querySelector('#rub');
+      
+inputRUB.addEventListener('input', (event)=>{
+    const request = new XMLHttpRequest();
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    request.addEventListener('load', ()=>{
+        if(request.status === 200){
+            const data = JSON.parse(request.response);
+            inputUSD.value = (+inputRUB.value/ data.current.usd).toFixed(2);
+        } else{
+            inputUSD.value = "Что-то пошдо не так";
+        }
+    });
+});
