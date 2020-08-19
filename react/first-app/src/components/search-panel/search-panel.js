44e7655cf909587072b39667bb38396bc06c5374
metkornet <60342504+metkornet@users.noreply.github.com>
2020-08-19 17:10:41 +0300

    Add the first React project

---
+++ react/first-app/src/components/search-panel/search-panel.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import './search-panel.css';
+
+export default class SearchPanel extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            term: ''
+        }
+        this.onSearch = this.onSearch.bind(this);
+    };
+
+    onSearch(e){
+        const term = e.target.value;
+        this.setState({term});
+        this.props.onUpdateSearch(term); 
+    }
+
+    render(){
+        return(
+            <input className = "form-control search-input" 
+            type = "text"
+            placeholder = "Поиск по записям"
+            onChange = {this.onSearch}
+            />
+        )
+    }
+    
+}
